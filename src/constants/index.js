eabb7e15ba010b77262c5bf1c9abb14b8664fb8d
tientg <nguyentien7566@gmail.com>
2020-05-29 23:35:14 +0700

    chia 3 cot giao dien trang them user

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const STATUSES = [
+    {
+        value: 0,
+        label: "USER CẦN THÊM"
+    },
+    {
+        value: 1,
+        label: "USER ĐANG CHỜ DUYỆT"
+    },
+    {
+        value: 2,
+        label: "USER ĐÃ THÊM THÀNH CÔNG"
+    }
+];
--- src/containers/TaskBoard/index.js
+++ src/containers/TaskBoard/index.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import styles from './styles.js';
 import { withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
+import { STATUSES } from '../../constants/index.js';
 
 class TaskBoard extends Component {
+    renderBoard() {
+        let xhtml = null;
+        xhtml = (
+            <Grid container spacing={2}>
+                {
+                    STATUSES.map(
+                        (status, index) => {
+                            return (
+                                <Grid item md={4} xs={12} key={status.value}>
+                                    {status.label}
+                                </Grid>
+                            );
+                        }
+                    )
+                }
+            </Grid>
+        );
+        return xhtml;
+    }
     render() {
-        const {classes} = this.props;
+        const { classes } = this.props;
         return (
-            <div className={classes.taskboard}>
-                <div className={classes.shape}>Mai</div>
-                <div className={classes.shape}>Lan</div>
+            <div className={classes.taskBoard}>
+                <Button variant="contained" color="primary" className={classes.button}>
+                    <AddIcon />
+                    Thêm user
+                </Button>
+                {this.renderBoard()}
             </div>
         )
     }
