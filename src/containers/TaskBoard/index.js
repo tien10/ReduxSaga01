1260e2d0f783517dcb7885078c76e183b073c784
tientg <nguyentien7566@gmail.com>
2020-06-01 22:37:05 +0700

    m

--- src/containers/TaskBoard/index.js
+++ src/containers/TaskBoard/index.js
@@ -6,17 +6,51 @@ import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 import { STATUSES } from '../../constants/index.js';
 
+const listTask = [
+    {
+        id: 1,
+        title: "Mai",
+        description: "Mai Ngoc",
+        status: 0
+    },
+    {
+        id: 2,
+        title: "Lan",
+        description: "Mai Ho Diep",
+        status: 2
+    },
+    {
+        id: 3,
+        title: "Cuc",
+        description: "Cuc Nguyen",
+        status: 1
+    }
+];
+
 class TaskBoard extends Component {
     renderBoard() {
         let xhtml = null;
+        const {classes} = this.props;
         xhtml = (
             <Grid container spacing={2}>
                 {
                     STATUSES.map(
                         (status, index) => {
+                            const taskFilterd = listTask.filter(task => task.status === status.value)
                             return (
                                 <Grid item md={4} xs={12} key={status.value}>
-                                    {status.label}
+                                    <h1 className={classes.status}>{status.label}</h1>
+                                    <div className={classes.wrapperListTask}>
+                                        {
+                                            listTask.map(
+                                                task => {
+                                                    return (
+                                                    <h1>{taskFilterd.title}</h1>
+                                                    )
+                                                }
+                                            )
+                                        }
+                                    </div>
                                 </Grid>
                             );
                         }
